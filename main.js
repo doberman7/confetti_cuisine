8f1682878887c9e185eb7889b6e975853b096cf9
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-09-13 14:15:11 -0500

    Moving user routes to userRoutes.js

--- main.js
+++ main.js
@@ -79,16 +79,6 @@ router.use((req, res, next) => {//With this middleware function, I have access t
 
 router.get("/", homeController.index);
 
-router.get("/users", usersController.index, usersController.indexView);
-router.get("/users/new", usersController.new);
-router.get("/users/login",  usersController.login);
-router.post("/users/login",usersController.authenticate);
-router.get("/users/logout", usersController.logout, usersController.redirectView);
-router.post("/users/create", usersController.validate, usersController.logEmail, usersController.create, usersController.redirectView);
-router.get("/users/:id/edit", usersController.edit);//Add routes to handle viewing.
-router.put("/users/:id/update", usersController.update, usersController.redirectView);//Process data from the edit form, and display the user show page
-router.delete ("/users/:id/delete", usersController.delete, usersController.redirectView);
-router.get("/users/:id", usersController.show, usersController.showView);
 
 router.get("/subscribers", subscribersController.index, subscribersController.indexView);
 router.get("/subscribers/new", subscribersController.new);
