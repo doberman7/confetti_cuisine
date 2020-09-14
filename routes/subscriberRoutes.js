f0486a9a61b21f3602eb2b68ff83d420eb7862b6
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-09-14 14:00:57 -0500

    subscribers CRUD routes edited

--- routes/subscriberRoutes.js
+++ routes/subscriberRoutes.js
@@ -1,13 +1,13 @@
 const router = require("express").Router(),
   subscribersController = require("../controllers/subscribersController");
 
-router.get("/subscribers", subscribersController.index, subscribersController.indexView);
-router.get("/subscribers/new", subscribersController.new);
-router.post("/subscribers/create", subscribersController.create,subscribersController.redirectView);
-router.get("/subscribers/:id/edit", subscribersController.edit);//Add routes to handle viewing.
-router.put("/subscribers/:id/update", subscribersController.update,
+router.get("/", subscribersController.index, subscribersController.indexView);
+router.get("/new", subscribersController.new);
+router.post("/create", subscribersController.create,subscribersController.redirectView);
+router.get("/:id/edit", subscribersController.edit);//Add routes to handle viewing.
+router.put("/:id/update", subscribersController.update,
  subscribersController.redirectView);//Process data from the edit form, and display the user show page
-router.delete ("/subscribers/:id/delete", subscribersController.delete, subscribersController.redirectView)
-router.get("/subscribers/:id", subscribersController.show, subscribersController.showView);
+router.delete ("/:id/delete", subscribersController.delete, subscribersController.redirectView)
+router.get("/:id", subscribersController.show, subscribersController.showView);
 
 module.exports = router;
